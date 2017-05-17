44931eac4b754506bc1269c821f36a5a1071db9f
brianhong <brianhong0510@gmail.com>
2017-05-17 18:11:37 -0400

    Added files

---
+++ browser/react/containers/PlaylistContainer.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export default class extends React.Component {
+  constructor(props) {
+    super();
+  }
+
+  render() {
+    return(
+      <div></div>
+    );
+  }
+}
