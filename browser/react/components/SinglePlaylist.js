bf0a6d49dc4c28f33578b57f8d29bbed4080890d
brianhong <brianhong0510@gmail.com>
2017-05-18 13:01:45 -0400

    Began adding songs

--- browser/react/components/SinglePlaylist.js
+++ browser/react/components/SinglePlaylist.js
@@ -21,6 +21,27 @@ export default class extends React.Component{
         <Songs songs={playlist.songs} />
         { playlist.songs && !playlist.songs.length && <small>No songs.</small> }
         <hr />
+        <div className="well">
+          <form className="form-horizontal" noValidate name="songSelect">
+          <fieldset>
+            <legend>Add to Playlist</legend>
+            <div className="form-group">
+              <label htmlFor="song" className="col-xs-2 control-label">Song</label>
+              <div className="col-xs-10">
+                <select className="form-control" name="song">
+                  <option value={'1'}>song name</option>
+                  <option value={'2'}>another song name</option>
+                </select>
+              </div>
+            </div>
+            <div className="form-group">
+              <div className="col-xs-10 col-xs-offset-2">
+                <button type="submit" className="btn btn-success">Add Song</button>
+              </div>
+            </div>
+          </fieldset>
+          </form>
+        </div>
       </div>
     );
   }
